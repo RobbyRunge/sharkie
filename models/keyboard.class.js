84645e773fc8f047b97ce5470c4e5977fd07c753
RobbyR98 <robbyrunge@yahoo.com>
2025-04-29 18:11:17 +0200

    refactor: move keyboard event handling to Keyboard class for better encapsulation

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -6,4 +6,58 @@ class Keyboard {
   SPACE = false;
   D = false;
   E = false; 
+
+  constructor() {
+    window.addEventListener('keydown', (event) => {  
+      switch(event.code) {
+        case 'ArrowLeft':
+          this.LEFT = true;
+          break;
+        case 'ArrowRight':
+          this.RIGHT = true;
+          break;
+        case 'ArrowUp':
+          this.UP = true;
+          break;
+        case 'ArrowDown':
+          this.DOWN = true;
+          break;
+        case 'Space':
+          this.SPACE = true;
+          break;
+        case 'KeyD':
+          this.D = true;
+          break;
+        case 'KeyE':
+          this.E = true;
+          break;
+      }
+    }); 
+
+    window.addEventListener('keyup', (event) => {
+      switch(event.code) {
+        case 'ArrowLeft':
+          this.LEFT = false;
+          break;
+        case 'ArrowRight':
+          this.RIGHT = false;
+          break;
+        case 'ArrowUp':
+          this.UP = false;
+          break;
+        case 'ArrowDown':
+          this.DOWN = false;
+          break;
+        case 'Space':
+          this.SPACE = false;
+          break;
+        case 'KeyD':
+          this.D = false;
+          break;
+        case 'KeyE':
+          this.E = false;
+          break;
+      }
+    });
+  }
 }
