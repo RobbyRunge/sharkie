6b550ab702ad7caee1538495a9ae0b4d242951c1
RobbyR98 <robbyrunge@yahoo.com>
2025-04-30 19:29:30 +0200

    refactor: remove passive option from touch event listeners for better compatibility

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -15,77 +15,77 @@ class GameKeyboard {
   bindKeyPressEvents() {
     document.getElementById('game_container').addEventListener('touchmove', (e) => {
       e.preventDefault();
-    }, { passive: false });
+    });
 
     document.getElementById('arrow_left').addEventListener('touchstart', (e) => {
       e.preventDefault();
       this.LEFT = true;
-    }, { passive: false });
+    });
 
     document.getElementById('arrow_left').addEventListener('touchend', (e) => {
       e.preventDefault();
       this.LEFT = false;
-    }, { passive: false });
+    });
 
     document.getElementById('arrow_up').addEventListener('touchstart', (e) => {
       e.preventDefault();
       this.UP = true;
-    }, { passive: false });
+    });
 
     document.getElementById('arrow_up').addEventListener('touchend', (e) => {
       e.preventDefault();
       this.UP = false;
-    }, { passive: false });
+    });
 
     document.getElementById('arrow_down').addEventListener('touchstart', (e) => {
       e.preventDefault();
       this.DOWN = true;
-    }, { passive: false });
+    });
 
     document.getElementById('arrow_down').addEventListener('touchend', (e) => {
       e.preventDefault();
       this.DOWN = false;
-    }, { passive: false });
+    });
 
     document.getElementById('arrow_right').addEventListener('touchstart', (e) => {
       e.preventDefault();
       this.RIGHT = true;
-    }, { passive: false });
+    });
 
     document.getElementById('arrow_right').addEventListener('touchend', (e) => {
       e.preventDefault();
       this.RIGHT = false;
-    }, { passive: false });
+    });
 
     document.getElementById('speed_boost').addEventListener('touchstart', (e) => {
       e.preventDefault();
       this.E = true;
-    }, { passive: false });
+    });
 
     document.getElementById('speed_boost').addEventListener('touchend', (e) => {
       e.preventDefault();
       this.E = false;
-    }, { passive: false });
+    });
 
     document.getElementById('throw_bottle').addEventListener('touchstart', (e) => {
       e.preventDefault();
       this.D = true;
-    }, { passive: false });
+    });
 
     document.getElementById('throw_bottle').addEventListener('touchend', (e) => {
       e.preventDefault();
       this.D = false;
-    }, { passive: false });
+    });
 
     document.getElementById('fin_slap').addEventListener('touchstart', (e) => {
       e.preventDefault();
       this.SPACE = true;
-    }, { passive: false });
+    });
 
     document.getElementById('fin_slap').addEventListener('touchend', (e) => {
       e.preventDefault();
       this.SPACE = false;
-    }, { passive: false });
+    });
   }
 
   bindButtonsPressEvents() {
